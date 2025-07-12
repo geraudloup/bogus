429478137ef3c8e2c3e853be36d5fad926ab01d9
Géraud-Loup <47665233+geraudloup@users.noreply.github.com>
2025-07-12 18:49:39 +1000

    Create bogus.py

---
+++ bogus.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(b"Hello, world!")
+
+if __name__ == "__main__":
+    port = 8020
+    server = HTTPServer(("", port), HelloHandler)
+    print(f"Serving on http://localhost:{port}")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nShutting down.")
+        server.server_close()
